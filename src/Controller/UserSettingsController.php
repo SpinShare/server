fc4c2b08ce5c5bb33b339743ea0261e7971beed1
SpinShare <wearespinshare@gmail.com>
2020-06-18 13:21:31 +0200

    added settings tabs

--- src/Controller/UserSettingsController.php
+++ src/Controller/UserSettingsController.php
@@ -27,35 +27,62 @@ class UserSettingsController extends AbstractController
         if($request->request->get('save')) {
             $formData = $request->request;
 
-            switch($request->request->get('save')) {
-                case "general":
-                    if($formData->get('email') != "" && $formData->get('username')) {
-                        $existingUser = $em->getRepository(User::class)->findOneBy(array('username' => $formData->get('username'), 'email' => $formData->get('email')));
+            if($formData->get('email') != "" && $formData->get('username')) {
+                $existingUser = $em->getRepository(User::class)->findOneBy(array('username' => $formData->get('username'), 'email' => $formData->get('email')));
 
-                        if($existingUser != $this->getUser()) {
-                            $user->setEmail($formData->get('email'));
-                            $user->setUsername($formData->get('username'));
-                            $this->addFlash('success', 'Saved successfully!');
-                        } else {
-                            $this->addFlash('error', 'Email or username are already in use!');
-                        }
-                    } else {
-                        $this->addFlash('error', 'Email and username can\'t be empty!');
-                    }
-                break;
-                case "login":
-                    if($formData->get('newPassword') == $formData->get('newPasswordAgain')) {
-                        $user->setPlainPassword($formData->get('newPassword'));
-                        $this->addFlash('success', 'Saved successfully!');
-                    } else {
-                        $this->addFlash('error', 'Passwords must be equal!');
-                    }
-                break;
+                if($existingUser != $user) {
+                    $user->setEmail($formData->get('email'));
+                    $user->setUsername($formData->get('username'));
+                    $this->addFlash('success', 'Saved successfully!');
+                } else {
+                    $this->addFlash('error', 'Email or username are already in use!');
+                }
+            } else {
+                $this->addFlash('error', 'Email and username can\'t be empty!');
             }
 
             $userManager->updateUser($user);
         }
 
-        return $this->render('user/settings.html.twig', $data);
+        return $this->render('user/settings/index.html.twig', $data);
+    }
+
+    /**
+     * @Route("/settings/security", name="user.settings.security")
+     */
+    public function userSettingsSecurity(Request $request, UserPasswordEncoderInterface $encoder, UserManagerInterface $userManager)
+    {
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $data = [];
+
+        $data['my'] = $user;
+
+        if($request->request->get('save')) {
+            $formData = $request->request;
+
+            if($formData->get('newPassword') == $formData->get('newPasswordAgain')) {
+                $user->setPlainPassword($formData->get('newPassword'));
+                $this->addFlash('success', 'Saved successfully!');
+            } else {
+                $this->addFlash('error', 'Passwords must be equal!');
+            }
+
+            $userManager->updateUser($user);
+        }
+
+        return $this->render('user/settings/security.html.twig', $data);
+    }
+    
+    /**
+     * @Route("/settings/login", name="user.settings.login")
+     */
+    public function userSettingsLogin(Request $request)
+    {
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $data = [];
+
+        return $this->render('user/settings/login.html.twig', $data);
     }
 }
