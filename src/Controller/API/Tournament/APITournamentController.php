84eb1dc7cd0873fc61e40a1a793778da02d2543a
SpinShare <wearespinshare@gmail.com>
2021-04-16 18:23:02 +0200

    update tournament playlist

--- src/Controller/API/Tournament/APITournamentController.php
+++ src/Controller/API/Tournament/APITournamentController.php
@@ -25,7 +25,7 @@ class APITournamentController extends AbstractController
         $baseUrl = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath();
 
         // Botch
-        $tournamentPlaylist = $em->getRepository(SongPlaylist::class)->findOneBy(array('id' => "84"));
+        $tournamentPlaylist = $em->getRepository(SongPlaylist::class)->findOneBy(array('id' => "146"));
 
         foreach($tournamentPlaylist->getSongs() as $tournamentChart) {
             $chartItem = $tournamentChart->getJSON();
