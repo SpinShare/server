04a4572f02144df46fce5f9367b65cdc3241ccf3
Laura Heimann <hello@laura.media>
2022-06-16 23:51:03 +0200

    Updated WarpZone

--- src/Controller/API/Tournament/APITournamentController.php
+++ src/Controller/API/Tournament/APITournamentController.php
@@ -25,7 +25,7 @@ class APITournamentController extends AbstractController
         $baseUrl = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath();
 
         // Botch
-        $tournamentPlaylist = $em->getRepository(SongPlaylist::class)->findOneBy(array('id' => "236"));
+        $tournamentPlaylist = $em->getRepository(SongPlaylist::class)->findOneBy(array('id' => "340"));
 
         foreach($tournamentPlaylist->getSongs() as $tournamentChart) {
             $chartItem = $tournamentChart->getJSON();
